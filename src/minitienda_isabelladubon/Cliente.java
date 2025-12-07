a0fbe23ee9aba6da208a7e236ccc2e7b0c698825
Isabella-Dubon <isabella_dubon@unitec.edu>
2025-12-07 02:39:55 -0600

    menu principal

--- src/minitienda_isabelladubon/Cliente.java
+++ src/minitienda_isabelladubon/Cliente.java
@@ -8,6 +8,37 @@ public class Cliente {
     private String nombre;
     private String numTelefono;
     private String direccion;
+
+    public Cliente(String nombre, String numTelefono, String direccion) {
+        this.nombre = nombre;
+        this.numTelefono = numTelefono;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNumTelefono() {
+        return numTelefono;
+    }
+
+    public void setNumTelefono(String numTelefono) {
+        this.numTelefono = numTelefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
     
     
 }
